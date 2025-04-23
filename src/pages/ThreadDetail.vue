dbe398a8a004c3d6f30253f3fc4eb42985ea23b1
link0125 <yodahara0125@gmail.com>
2025-04-23 20:48:14 +0900

    見た目キレイに

--- src/pages/ThreadDetail.vue
+++ src/pages/ThreadDetail.vue
@@ -5,24 +5,24 @@
       :class="[$q.screen.lt.md ? 'q-px-sm' : 'q-px-xl q-mx-lg']"
     >
       <div v-if="fetching">
-        <div class="row justify-center">
-          <q-spinner-orbit color="primary" size="3em" class="q-my-lg" />
+        <div class="row justify-center q-my-lg">
+          <q-spinner-orbit color="primary" size="3em" />
         </div>
       </div>
       <div v-else>
-        <div class="text-h4 q-py-xl">{{ thread.title }}</div>
-        <div class="text-grey q-pb-md">
+        <div class="text-h4 q-mb-md q-mt-xl">{{ thread.title }}</div>
+        <div class="text-grey q-mb-md">
           {{ thread.humanized_created_at }} {{ thread.user.username }}
         </div>
-        <div v-html="thread.body" class="text-body1"></div>
+        <div v-html="thread.body" class="text-body1 q-mb-lg"></div>
       </div>
       <div v-if="commentFetching">
-        <div class="row justify-center">
-          <q-spinner-orbit color="primary" size="3em" class="q-my-lg" />
+        <div class="row justify-center q-my-lg">
+          <q-spinner-orbit color="primary" size="3em" />
         </div>
       </div>
-      <div v-else>
-        <div v-for="comment in comments" :key="comment.id">
+      <div v-else style="width: 100%">
+        <div v-for="comment in comments" :key="comment.id" class="q-mb-md">
           <div class="row justify-start items-start q-py-md">
             <q-img
               :src="comment.user.avatar_image"
@@ -36,32 +36,123 @@
               "
             >
             </q-img>
-            <div class="q-pl-md">
-              <div class="q-py-sm">{{ comment.user.username }}</div>
-              <div class="text-body1">
+
+            <div style="width: 90%" class="q-pl-sm">
+              <div class="q-mb-xs">{{ comment.user.username }}</div>
+              <div class="text-body1 q-mb-sm">
                 {{ comment.body }}
               </div>
-              <div class="text-grey-5 q-py-sm">
-                {{ comment.humanized_created_at }} 返信
+              <div class="row justify-between q-mb-sm">
+                <small class="text-grey-5">
+                  {{ comment.humanized_created_at }} 返信
+                </small>
+                <q-btn
+                  flat
+                  dense
+                  round
+                  color="grey"
+                  icon="close"
+                  size="xs"
+                  style="padding: 0; height: auto; width: auto"
+                >
+                  <q-menu>
+                    <q-list dense style="min-width: 100px">
+                      <q-item
+                        clickable
+                        v-close-popup
+                        style="padding: 12px"
+                        @click="console.log('clicked')"
+                      >
+                        <q-item-section
+                          >非表示
+                          <q-item-label caption
+                            >この投稿が表示されなくなります</q-item-label
+                          >
+                        </q-item-section>
+                      </q-item>
+                      <q-item clickable v-close-popup style="padding: 12px">
+                        <q-item-section
+                          >このユーザーを表示しない
+                          <q-item-label caption
+                            >このユーザーの投稿が表示されなくなります</q-item-label
+                          >
+                        </q-item-section>
+                      </q-item>
+
+                      <q-separator />
+                      <q-item clickable style="padding: 12px">
+                        <q-item-section
+                          >報告
+                          <q-item-label caption>違反を報告します</q-item-label>
+                        </q-item-section>
+                        <q-item-section side>
+                          <q-icon name="keyboard_arrow_right" />
+                        </q-item-section>
+
+                        <q-menu anchor="top end" self="top start">
+                          <q-list>
+                            <q-item clickable style="padding: 9px">
+                              <q-item-section>コミュニティ違い</q-item-section>
+                            </q-item>
+                            <q-item clickable style="padding: 9px">
+                              <q-item-section>禁止行為</q-item-section>
+                            </q-item>
+                            <q-item clickable style="padding: 9px">
+                              <q-item-section>スパム/過剰投稿</q-item-section>
+                            </q-item>
+                          </q-list>
+                        </q-menu>
+                      </q-item>
+                      <q-separator />
+                    </q-list>
+                  </q-menu>
+                </q-btn>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <q-separator class="q-my-md" />
+      <div class="q-mb-md">コメント</div>
       <q-input
         v-model="comment"
-        filled
-        clearable
+        outlined
         type="textarea"
         color="blue-12"
-        label="コメントを投稿..."
+        :rows="5"
+        class="q-mb-md"
       />
-      <q-btn
-        class="q-mt-md"
-        label="投稿"
-        color="green-9"
-        @click="postComment"
-      />
+      <div v-if="isLoggedIn">
+        <q-btn
+          class="q-mt-md"
+          label="投稿"
+          color="green-9"
+          @click="postComment"
+        />
+      </div>
+      <div v-else>
+        <div class="q-mt-md" style="width: 250px">
+          <q-input outlined v-model="username" lazy-rules dense />
+
+          <q-input
+            outlined
+            type="password"
+            v-model="password"
+            lazy-rules
+            dense
+            class="q-my-md"
+          />
+        </div>
+
+        <q-btn
+          class="q-mt-md"
+          label="ログインして投稿"
+          color="green-9"
+          @click="postComment"
+        />
+      </div>
+
+      <div class="q-my-xl"></div>
     </div>
     <div class="col-2">
       <div v-if="fetching"></div>
@@ -93,7 +184,9 @@ import { useRoute } from 'vue-router';
 import { onMounted, computed } from 'vue';
 import { useThreadStore } from 'stores/thread';
 import { useUserStore } from 'src/stores/user';
-
+import { is, useQuasar } from 'quasar';
+import { ref } from 'vue';
+const $q = useQuasar();
 const route = useRoute();
 const threadId = route.params.id as string;
 const store = useThreadStore();
@@ -103,13 +196,51 @@ const fetching = computed(() => store.getFetching);
 const commentFetching = computed(() => store.getCommentFetching);
 const thread = computed(() => store.thread);
 const comments = computed(() => store.comments);
-let comment = '';
+const isLoggedIn = computed(() => userStore.is_logged_in);
+
+const comment = ref('');
+const username = ref('');
+const password = ref('');
+
 const fetchThread = () => store.fetchThread(threadId);
 const fetchComments = () => store.fetchComments(threadId);
-const postComment = () => {
-  store.postComment(threadId, comment, access_token);
-  comment = '';
-};
+
+async function postComment() {
+  if (comment.value.length > 0) {
+    try {
+      const response = await store.postComment(
+        threadId,
+        comment.value,
+        access_token
+      );
+
+      if (response.status === 201) {
+        comment.value = '';
+      } else {
+        $q.notify({
+          message: `投稿に失敗しました（ステータス: ${response.status}）`,
+          color: 'red',
+          position: 'bottom',
+          timeout: 2000,
+        });
+      }
+    } catch (error) {
+      $q.notify({
+        message: 'コメント投稿中にエラーが発生しました',
+        color: 'red',
+        position: 'bottom',
+        timeout: 2000,
+      });
+    }
+  } else {
+    $q.notify({
+      message: 'コメントを入力してください',
+      color: 'red',
+      position: 'bottom',
+      timeout: 2000,
+    });
+  }
+}
 
 onMounted(() => {
   fetchThread();
